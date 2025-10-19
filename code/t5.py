4199856c256b8f0b6a1942c923ca369d1a221319
ANC13NT68 <tasher393@gmail.com>
2025-10-19 22:52:03 +0500

    Add files via upload

---
+++ code/t5.py
@@ -0,0 +1,15 @@
+products = ["молоко", "хлеб", "яйца", "сыр"]
+
+print("Тест 1 - Начальный список:")
+print(products)
+print()
+
+print("Тест 2 - Добавляем продукты:")
+products.append("колбаса")
+products.append("сок")
+print(products)
+print()
+
+print("Тест 3 - Удаляем продукты:")
+products.remove("хлеб")
+print(products)
